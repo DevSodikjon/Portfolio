0e739d243ecac4691288fed1a8b66f1aa310f450
Sodikjon Nurmirzaev <devsodikjon2003@gmail.com>
2025-07-17 03:31:08 +0900

    projects section

--- components/main/Projects.tsx
+++ components/main/Projects.tsx
@@ -1,32 +1,88 @@
+'use client';
 import React from "react";
 import ProjectCard from "../sub/ProjectCard";
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { EffectCoverflow, Pagination } from 'swiper/modules';
+
+import 'swiper/css';
+import 'swiper/css/effect-coverflow';
+import 'swiper/css/pagination';
+
 
 const Projects = () => {
   return (
     <div
-      className="flex flex-col items-center justify-center py-20"
+      className="w-full flex flex-col items-center justify-center py-20"
       id="projects"
     >
-      <h1 className="text-[40px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-20">
+      <h1 className="text-[40px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 py-20">
         My Projects
       </h1>
-      <div className="h-full w-full flex flex-col md:flex-row gap-10 px-10">
-        <ProjectCard
-          src="/NextWebsite.png"
-          title="Modern Next.js Portfolio"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-        />
-        <ProjectCard
-          src="/CardImage.png"
-          title="Interactive Website Cards"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-        />
-        <ProjectCard
-          src="/SpaceWebsite.png"
-          title="Space Themed Website"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-        />
-      </div>
+
+      <Swiper
+        effect="coverflow"
+        grabCursor={true}
+        centeredSlides={true}
+        slidesPerView="auto"
+        coverflowEffect={{
+          rotate: 50,
+          stretch: 0,
+          depth: 100,
+          modifier: 1,
+          slideShadows: true,
+        }}
+        pagination={{ clickable: true }}
+        modules={[EffectCoverflow, Pagination]}
+        className="swiper w-full max-w-6xl px-4 "
+      >
+
+        <SwiperSlide className="swiper w-[200px] h-[200px] rounded-lg overflow-hidden">
+          <img
+            src="https://swiperjs.com/demos/images/nature-1.jpg"
+            alt="Slide 1"
+            className="w-full h-full object-cover cursor-pointer"
+          />
+        </SwiperSlide>
+        <SwiperSlide className="w-[200px] h-[200px] rounded-lg overflow-hidden">
+          <img
+            src="https://swiperjs.com/demos/images/nature-1.jpg"
+            alt="Slide 1"
+            className="w-full h-full object-cover"
+          />
+        </SwiperSlide>
+        <SwiperSlide className="w-[200px] h-[200px] rounded-lg overflow-hidden">
+          <img
+            src="https://swiperjs.com/demos/images/nature-1.jpg"
+            alt="Slide 1"
+            className="w-full h-full object-cover"
+          />
+        </SwiperSlide>
+        <SwiperSlide className="w-[200px] h-[200px] rounded-lg overflow-hidden">
+          <img
+            src="https://swiperjs.com/demos/images/nature-1.jpg"
+            alt="Slide 1"
+            className="w-full h-full object-cover"
+          />
+        </SwiperSlide>
+        <SwiperSlide className="w-[200px] h-[200px] rounded-lg overflow-hidden">
+          <img
+            src="https://swiperjs.com/demos/images/nature-1.jpg"
+            alt="Slide 1"
+            className="w-full h-full object-cover"
+          />
+        </SwiperSlide>
+        <SwiperSlide className="w-[200px] h-[200px] rounded-lg overflow-hidden">
+          <img
+            src="https://swiperjs.com/demos/images/nature-1.jpg"
+            alt="Slide 1"
+            className="w-full h-full object-cover"
+          />
+        </SwiperSlide>
+
+      </Swiper>
+
+
     </div>
   );
 };
